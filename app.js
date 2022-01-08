a2e3afe43c884e363445593dace1e9aa6216b6c4
yeezick <ramerick5@gmail.com>
2022-01-08 10:57:24 -0500

    add a conditional method from inquirer to first ask the suer if they want an about section before aking them to fill that question in

--- app.js
+++ app.js
@@ -29,9 +29,17 @@ const promptUser = () => {
       },
     },
     {
+      type: "confirm",
+      name: "confirmAbout",
+      message:
+        'Would you like to enter some information about yourself for an "About" section?',
+      default: true,
+    },
+    {
       type: "input",
       name: "about",
       message: "provide some information about yourself",
+      when: ({ confirmAbout }) => confirmAbout,
     },
   ]);
 };
