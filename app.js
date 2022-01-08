c1c973a2397b2e1624815b05b6b13f690498629c
yeezick <ramerick5@gmail.com>
2022-01-08 10:43:25 -0500

    use inquirer to create a prompt which asks the user for my name then for information on their project. this is currently chained to recursively trigger the promptProject function in the event the user would like to add mroe than 1 project

--- app.js
+++ app.js
@@ -1,14 +1,93 @@
 const inquirer = require("inquirer");
 
-inquirer
-  .prompt([
+const promptUser = () => {
+  return inquirer.prompt([
     {
       type: "input",
       name: "name",
       message: "what is your name?",
     },
-  ])
-  .then((answers) => console.log(answers));
+    {
+      type: "input",
+      name: "github",
+      message: "what is your github?",
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "provide some information about yourself",
+    },
+  ]);
+};
+
+const promptProject = (portfolioData) => {
+  // since this function would have to be called again to add another project, this if condition stops node from redefining the array at every call
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+
+  console.log(`
+  =====================
+  Add to a new project!
+  =====================`);
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "what is the name of your project?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "provide a description of the project (required)",
+      },
+      {
+        type: "checkbox",
+        name: "languages",
+        message: "what did you make this project with? (check all that apply)",
+        choices: [
+          "javascript",
+          "html",
+          "css",
+          "es6",
+          "jquery",
+          "bootstrap",
+          "node",
+        ],
+      },
+      {
+        type: "input",
+        name: "link",
+        message: "enter the github link for your project. (Required)",
+      },
+      {
+        type: "confirm",
+        name: "feature",
+        message: "would you like to feature this project?",
+        default: false,
+      },
+      {
+        type: "confirm",
+        name: "confirmAddProject",
+        message: "Would you like to enter another project?",
+        default: false,
+      },
+    ])
+    .then((projectData) => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
+};
+
+promptUser()
+  .then(promptProject)
+  .then((portfolioData) => console.log(portfolioData));
+
 // const fs = require("fs");
 
 // const generatePage = require("./src/page-template.js");
